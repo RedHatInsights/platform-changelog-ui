1bc529365e38115c0a8e5ba5335dbf00be9cb4d8
Stephen Adams <stephen.adams@redhat.com>
2021-12-01 11:38:11 -0500

    [task] Add gitlab endpoint
    This works. There's a ton of duplicate code between this and github, so
    I think there is room for improvement here
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- api/config.py
+++ api/config.py
@@ -7,3 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GITHUB_SECRET = os.getenv("GITHUB_SECRET", "")
+    GITLAB_SECRET = os.getenv("GITLAB_SECRET", "")
