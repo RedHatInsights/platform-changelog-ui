29fbb71e9ea0f59cce4c99d8ee10b5a938bffa2e
Keith Walsh <kwalsh@redhat.com>
2021-12-01 16:17:51 -0500

    Factor out a reusable table component

--- ui/src/CommitTable.js
+++ ui/src/CommitTable.js
@@ -1,43 +1,10 @@
 import React from 'react';
-import {
-  Table,
-  TableHeader,
-  TableBody,
-  TableVariant
-} from '@patternfly/react-table';
+import GenericTable from './GenericTable';
 
 class CommitTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      serviceColumns: [],
-      serviceRows: []
-    }
-  }
-
-  componentDidMount() {
-    this.GetServices();
-  }
-
-  GetServices() {
-    fetch('/commits/').then(res => res.json()).then(data => {
-      if (data.length > 0) {
-        this.setState({
-          serviceColumns: Object.keys(data[0]),
-          serviceRows: data.map(d => Object.values(d))
-        });
-      }
-    });
-  }
-
   render() {
-    const serviceRows = this.state.serviceRows;
-    const serviceColumns = this.state.serviceColumns;
     return (
-      <Table caption="Commmits" rows={serviceRows} cells={serviceColumns} variant={TableVariant.compact}>
-        <TableHeader />
-        <TableBody />
-      </Table>
+      <GenericTable title="Commits" path="/commits/" />
     );
   }
 }
