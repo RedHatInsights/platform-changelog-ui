5814e02a0c6eebf75c036de60cabe6507528affc
Justin Orringer <justinorringer@gmail.com>
2022-11-01 12:36:29 -0400

    Removed improper linting

--- pr_check.sh
+++ pr_check.sh
@@ -4,15 +4,7 @@ APP_NAME="platform-changelog"
 COMPONENT_NAME="platform-changelog-ui"
 IMAGE="quay.io/cloudservices/platform-changelog-ui"
 
-# lint the project. We need more but this is all we have for now
-npm install
-npm run lint
-
-if [ $? -eq 0 ]; then
-    echo "Linting Success"
-else
-    exit 1
-fi
+# TODO: lint the project
 
 # create a 'dummy' result file so Jenkins will not fail
 mkdir -p $WORKSPACE/artifacts
