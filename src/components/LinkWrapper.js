f4c4550c2b0a33da7435595ca66b4498a0f198df
Justin Orringer <justinorringer@gmail.com>
2023-01-23 11:16:06 -0500

    Cleaned up LinkWrapper

--- src/components/LinkWrapper.js
+++ src/components/LinkWrapper.js
@@ -5,18 +5,10 @@ import { Link } from "react-router-dom";
 import { APP_ROOT } from '../AppConstants';
 
 // wrapper link function to include APP_ROOT
-// pass all props to Link
-// including children
-const LinkWrapper = (props) => {
+function LinkWrapper({ to, children }) {
     return (
-        <Link to={`${APP_ROOT}${props.to}`}>{props.children}</Link>
+        <Link to={`${APP_ROOT}${to}`}>{children}</Link>
     )
 }
-
-// function LinkWrapper({ to, children }) {
-//     return (
-//         <Link to={`${APP_ROOT}${to}`}>{children}</Link>
-//     )
-// }
 
 export default LinkWrapper;
